8a38ec225fe0c7bb67e68908931a2e54d083c6de
MSzoot <97002088+MSzoot@users.noreply.github.com>
2024-07-01 13:27:16 +0100

    asd

--- apiKey.jsx
+++
@@ -1,5 +0,0 @@
-const ApiKey = `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`;
-
-export default ApiKey;
-
-// echo -e "const ApiKey = \`Bearer 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\`;\n\nexport default ApiKey;" > apiKey.js
